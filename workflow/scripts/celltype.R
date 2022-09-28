48357f09158721c0d4cc4d9041a664946ffb0810
raysinensis <raysinensis@gmail.com>
2022-09-28 12:55:49 -0400

    gene dictionary to reference

--- workflow/scripts/celltype.R
+++ workflow/scripts/celltype.R
@@ -9,33 +9,49 @@ parser$add_argument('--input_h5', '-i', help = 'Input H5AD file containing prote
 parser$add_argument('--output', '-o', help = 'Output file')
 parser$add_argument('--ref', '-r', help = 'reference object')
 parser$add_argument('--typecol', '-c', help = 'celltype column in reference metadata')
+parser$add_argument('--genedict', '-d', help = 'gene dictionary if protein/antibody name is different in reference')
 xargs <- parser$parse_args()
 # load
 file_h5 <- H5File$new(xargs$input_h5, mode = "r+")
 mat <- file_h5[["X"]][,]
 colnames(mat) <- file_h5[["obs/_index"]][]
 rownames(mat) <- file_h5[["var/_index"]][]
+file_h5$close_all()
+
+# convert names
 prot <- mat[str_detect(rownames(mat), "^[A-Z]"),]
-rownames(prot) <- rownames(prot) %>% str_to_title() %>%
-  ifelse(. == "Lmn1b", "Lmnb1", .) %>%
-  ifelse(. == "Iba1", "Aif1", .) %>%
-  ifelse(. == "Nfh", "Nefh", .)
+if (!is.null(xargs$genedict)) {
+  genedict <- read_csv(xargs$genedict) %>% column_to_rownames("prot")
+  rownames(prot) <- sapply(rownames(prot), function(x) {
+    if (x %in% rownames(genedict)) {
+      return(genedict[x, "ref"])
+    } else {
+      return(x)
+    }
+  })
+}
+
+#rownames(prot) <- rownames(prot) %>% str_to_title() %>%
+#  ifelse(. == "Lmn1b", "Lmnb1", .) %>%
+#  ifelse(. == "Iba1", "Aif1", .) %>%
+#  ifelse(. == "Nfh", "Nefh", .)
+
 param <- mat[!str_detect(rownames(mat), "^[A-Z]"),]
 # combat
-mod <- model.matrix(~as.factor(label), data = as.data.frame(t(param)))
-mod0 <- model.matrix(~1,data = as.data.frame(t(param)))
-svobj <- sva(prot, mod, mod0, n.sv = 1, method = "two-step")
-if (svobj$n.sv > 0) {
-  prot2 <- ComBat(prot, batch = svobj$sv)
-} else {
+#mod <- model.matrix(~as.factor(label), data = as.data.frame(t(param)))
+#mod0 <- model.matrix(~1,data = as.data.frame(t(param)))
+#svobj <- sva(prot, mod, mod0, n.sv = 1, method = "two-step")
+#if (svobj$n.sv > 0) {
+#  prot2 <- ComBat(prot, batch = svobj$sv)
+#} else {
   prot2 <- prot
-}
-svobj <- sva(param, mod, mod0, n.sv = 1, method = "two-step")
-if (svobj$n.sv > 0) {
-  param2 <- ComBat(param, batch = svobj$sv)
-} else {
+#}
+#svobj <- sva(param, mod, mod0, n.sv = 1, method = "two-step")
+#if (svobj$n.sv > 0) {
+#  param2 <- ComBat(param, batch = svobj$sv)
+#} else {
   param2 <- param
-}
+#}
 # prot
 so <- CreateSeuratObject(prot2, assay = "prot")
 so <- NormalizeData(so, normalization.method = "CLR")
@@ -71,5 +87,15 @@ anchors <- suppressWarnings(FindTransferAnchors(reference = ref,
 pred <- TransferData(anchors, ref@meta.data[[cell_type]], k.weight = 10, weight.reduction = so[["pca"]], dims = 1:ncol(so@reductions$pca@cell.embeddings))
 so$type <- pred$predicted.id
 write_csv(so@meta.data %>% rownames_to_column("id") %>% select(id, type), xargs$output)
+
+# write h5ad
+file_h5 <- H5File$new(xargs$input_h5, mode = "r+")
+dtype_string_utf8 <- H5T_STRING$new()$set_size(Inf)$set_cset("UTF-8")
+#file_h5[["obs"]][["celltype"]] <- as.character(so$type)
+file_h5[["obs"]]$create_dataset(so$type, name = "celltype", dtype = dtype_string_utf8)
+h5attr(file_h5[["obs"]], "column-order") <- 
+  tryCatch(c(h5attr(file_h5[["obs"]], "column-order"), c("celltype")) %>% unique(),
+           error=function(e) c("celltype"))
+file_h5$close_all()
 
 }
