6c356282fea1dd7d11ae5e01a687666f040d35e4
Tina Liu <tinam@midnight-black-tea.local>
2023-05-01 16:32:49 -0400

    Updated dashboard.py to serve local folder

--- dashboard.py
+++ dashboard.py
@@ -5,17 +5,23 @@ import pandas as pd
 import os
 import dash
 from dash import dcc, ctx
-from dash import html 
+from dash import html, Dash 
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+from dash_canvas import DashCanvas
+import flask
 
 app = dash.Dash(__name__)
 server = app.server
 
-# locate folder of interest
-### !!! folder naming? can it be named something other than "assets"
-folders = [f for f in os.listdir('.') if os.path.isdir(f)]
 
+# point Dash to image directory of interest here
+image_directory = '/Users/tinam/Desktop/images'
+list_of_images = [f for f in os.listdir(image_directory) if '.png' in f]
+static_image_route = '/static/'
+
+# locate folder of interest
+# folders = [f for f in os.listdir('.') if os.path.isdir(f)]
 #image_paths=["assets/" + x for x in os.listdir("assets") if ".jpg" in x or ".jpeg" in x]
 
 ### !!! use the canvas component to display 
@@ -25,8 +31,34 @@ folders = [f for f in os.listdir('.') if os.path.isdir(f)]
 ### !!! ^ pytest
 
 app.layout = html.Div([
-    html.H1("Image Thumbnails - 17 Apr"), 
+    html.H1("Image Thumbnails - 2 May"), 
+    html.Div(["Folder being served: ", image_directory]),
     html.Div([
+        "Select the image you want to display",
+        dcc.Dropdown(
+            id="image-dropdown",
+            options=list_of_images,
+            placeholder="Select the image you want to display",
+        ),
+    ]),
+    html.Button('Previous Image', id='prev',n_clicks=0),
+    html.Button('Next Image', id='next',n_clicks=0),
+    html.Div(
+        #html.Img(src=image_paths[0]),
+        id='container-image'
+    ),
+    html.Div([
+        DashCanvas(id='canvas-image',
+            tool='line',
+            lineWidth=5,
+            lineColor='red',
+            image_content=static_image_route+list_of_images[0]
+            ),
+    ]),           
+])
+'''
+    # folder dropdown 
+        html.Div([
         "Select the folder with your images:",
         dcc.Dropdown(
             id="folder-dropdown",
@@ -37,23 +69,9 @@ app.layout = html.Div([
     ]), 
     #list files in selected folder in a bulleted list
     #html.Div(id="folder-files"),
-    html.Div([
-        "Select the image you want to display",
-        dcc.Dropdown(
-            id="image-dropdown",
-            #options=[{"label": x, "value": x} for x in image_paths],
-            #value=image_paths[0],
-            placeholder="Select the image you want to display",
-        ),
-    ]),
-    html.Button('Previous Image', id='prev',n_clicks=0),
-    html.Button('Next Image', id='next',n_clicks=0),
-    html.Div(
-        #html.Img(src=image_paths[0]),
-        id='container-image'
-    ),              
-])
+'''
 
+'''
 # Update image-dropdown depending on folder selected 
 @app.callback(Output("image-dropdown", "options"), Input("folder-dropdown", "value"))
 def update_image_dropdown(folder_name):
@@ -61,38 +79,48 @@ def update_image_dropdown(folder_name):
     #list all files in bulleted list
     #file_list = html.Ul([html.Li(file) for file in file_names])
     return images
+'''
+# Serve static images
+@app.server.route('{}<image_path>.png'.format(static_image_route))
+def serve_image(image_path):
+    image_name = '{}.png'.format(image_path)
+    if image_name not in list_of_images:
+        raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
+    return flask.send_from_directory(image_directory, image_name)
 
-# Display image selected in image-dropdown, utilize buttons to click through
+# Update canvas image based on image selected in image-dropdown
 @app.callback(
-    Output("container-image", "children"), 
+    Output("canvas-image", "image_content"), 
     Output("image-dropdown","value"),
-    Input("folder-dropdown","value"),
+    #Input("folder-dropdown","value"),
     Input("image-dropdown", "value"),
     Input("image-dropdown","options"),
     Input("next","n_clicks"),
     Input("prev","n_clicks"),
 )
-def display_image(folder,image,image_names,next,prev):
+def display_image(image,image_names,next,prev):
     if image is None:
         raise PreventUpdate
 
     if "image-dropdown" == ctx.triggered_id:
-        return html.Img(src=folder+"/"+image),image
-    #add cases for pressing next/prev when no image is selected    
+        return static_image_route+image,image
+    # !! add cases for pressing next/prev when no image is selected    
     elif "next" == ctx.triggered_id:
         index = image_names.index(image)
         if index < len(image_names)-1:
             index += 1
-            return html.Img(src=folder+"/"+image_names[index]),image_names[index]
+            return static_image_route+image_names[index],image_names[index]
         else:
-            return html.Img(src=folder+"/"+image_names[0]),image_names[0]
+            return static_image_route+image_names[0],image_names[0]
     elif "prev" == ctx.triggered_id:
         index = image_names.index(image)
         if index > 0:
             index -= 1
-            return html.Img(src=folder+"/"+image_names[index]),image_names[index]
+            return static_image_route+image_names[index],image_names[index]
         else:
-            return html.Img(src=folder+"/"+image_names[len(image_names)-1]),image_names[len(image_names)-1]
+            return static_image_route+image_names[len(image_names)-1],image_names[len(image_names)-1]
+
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
@@ -140,6 +168,41 @@ def display_all_images(folder_name):
     image_list = html.Ul([html.Li(html.Img(src = folder_name + '/'+ file for file in file_names))])
 
     return image_list
+
+'''
+
+'''
+# Display image selected in image-dropdown, utilize buttons to click through
+@app.callback(
+    Output("container-image", "children"), 
+    Output("image-dropdown","value"),
+    Input("folder-dropdown","value"),
+    Input("image-dropdown", "value"),
+    Input("image-dropdown","options"),
+    Input("next","n_clicks"),
+    Input("prev","n_clicks"),
+)
+def display_image(folder,image,image_names,next,prev):
+    if image is None:
+        raise PreventUpdate
+
+    if "image-dropdown" == ctx.triggered_id:
+        return html.Img(src=folder+"/"+image),image
+    #add cases for pressing next/prev when no image is selected    
+    elif "next" == ctx.triggered_id:
+        index = image_names.index(image)
+        if index < len(image_names)-1:
+            index += 1
+            return html.Img(src=folder+"/"+image_names[index]),image_names[index]
+        else:
+            return html.Img(src=folder+"/"+image_names[0]),image_names[0]
+    elif "prev" == ctx.triggered_id:
+        index = image_names.index(image)
+        if index > 0:
+            index -= 1
+            return html.Img(src=folder+"/"+image_names[index]),image_names[index]
+        else:
+            return html.Img(src=folder+"/"+image_names[len(image_names)-1]),image_names[len(image_names)-1]
 
 '''
 
